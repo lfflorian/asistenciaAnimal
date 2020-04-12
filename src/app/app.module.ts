d6049692944c6085af8329d400ab01dfc59316bb
lfflorian <lfflorian4@gmail.com>
2020-04-11 22:32:47 -0600

    correción de rutas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,14 +10,14 @@ import { ComponentsModule } from './components/components.module';
 
 import { AppComponent } from './app.component';
 
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { UserProfileComponent } from './user-profile/user-profile.component';
-import { TableListComponent } from './table-list/table-list.component';
-import { TypographyComponent } from './typography/typography.component';
-import { IconsComponent } from './icons/icons.component';
-import { MapsComponent } from './maps/maps.component';
-import { NotificationsComponent } from './notifications/notifications.component';
-import { UpgradeComponent } from './upgrade/upgrade.component';
+import { DashboardComponent } from './admin/dashboard/dashboard.component';
+import { UserProfileComponent } from './admin/user-profile/user-profile.component';
+import { TableListComponent } from './admin/table-list/table-list.component';
+import { TypographyComponent } from './admin/typography/typography.component';
+import { IconsComponent } from './admin/icons/icons.component';
+import { MapsComponent } from './admin/maps/maps.component';
+import { NotificationsComponent } from './admin/notifications/notifications.component';
+import { UpgradeComponent } from './admin/upgrade/upgrade.component';
 import {
   AgmCoreModule
 } from '@agm/core';
--- src/app/layouts/admin-layout/admin-layout.module.ts
+++ src/app/layouts/admin-layout/admin-layout.module.ts
@@ -3,14 +3,14 @@ import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AdminLayoutRoutes } from './admin-layout.routing';
-import { DashboardComponent } from '../../dashboard/dashboard.component';
-import { UserProfileComponent } from '../../user-profile/user-profile.component';
-import { TableListComponent } from '../../table-list/table-list.component';
-import { TypographyComponent } from '../../typography/typography.component';
-import { IconsComponent } from '../../icons/icons.component';
-import { MapsComponent } from '../../maps/maps.component';
-import { NotificationsComponent } from '../../notifications/notifications.component';
-import { UpgradeComponent } from '../../upgrade/upgrade.component';
+import { DashboardComponent } from '../../admin/dashboard/dashboard.component';
+import { UserProfileComponent } from '../../admin/user-profile/user-profile.component';
+import { TableListComponent } from '../../admin/table-list/table-list.component';
+import { TypographyComponent } from '../../admin/typography/typography.component';
+import { IconsComponent } from '../../admin/icons/icons.component';
+import { MapsComponent } from '../../admin/maps/maps.component';
+import { NotificationsComponent } from '../../admin/notifications/notifications.component';
+import { UpgradeComponent } from '../../admin/upgrade/upgrade.component';
 
 import {
   MatButtonModule,
