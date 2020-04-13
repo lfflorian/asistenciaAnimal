c7dffa74c7ef3379a0573c48462ccbf0cac68e21
lfflorian <lfflorian4@gmail.com>
2020-04-13 13:40:51 -0600

    se eliminan los componentes inecesarios

--- src/app/layouts/admin-layout/admin-layout.module.ts
+++ src/app/layouts/admin-layout/admin-layout.module.ts
@@ -3,14 +3,6 @@ import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AdminLayoutRoutes } from './admin-layout.routing';
-import { DashboardComponent } from '../../admin/dashboard/dashboard.component';
-import { UserProfileComponent } from '../../admin/user-profile/user-profile.component';
-import { TableListComponent } from '../../admin/table-list/table-list.component';
-import { TypographyComponent } from '../../admin/typography/typography.component';
-import { IconsComponent } from '../../admin/icons/icons.component';
-import { MapsComponent } from '../../admin/maps/maps.component';
-import { NotificationsComponent } from '../../admin/notifications/notifications.component';
-import { UpgradeComponent } from '../../admin/upgrade/upgrade.component';
 
 import {
   MatButtonModule,
@@ -34,14 +26,6 @@ import {
     MatTooltipModule,
   ],
   declarations: [
-    DashboardComponent,
-    UserProfileComponent,
-    TableListComponent,
-    TypographyComponent,
-    IconsComponent,
-    MapsComponent,
-    NotificationsComponent,
-    UpgradeComponent,
   ]
 })
 
--- src/app/layouts/admin-layout/admin-layout.routing.ts
+++ src/app/layouts/admin-layout/admin-layout.routing.ts
@@ -1,14 +1,5 @@
 import { Routes } from '@angular/router';
 
-import { DashboardComponent } from '../../admin/dashboard/dashboard.component';
-import { UserProfileComponent } from '../../admin/user-profile/user-profile.component';
-import { TableListComponent } from '../../admin/table-list/table-list.component';
-import { TypographyComponent } from '../../admin/typography/typography.component';
-import { IconsComponent } from '../../admin/icons/icons.component';
-import { MapsComponent } from '../../admin/maps/maps.component';
-import { NotificationsComponent } from '../../admin/notifications/notifications.component';
-import { UpgradeComponent } from '../../admin/upgrade/upgrade.component';
-
 export const AdminLayoutRoutes: Routes = [
     // {
     //   path: '',
@@ -52,12 +43,5 @@ export const AdminLayoutRoutes: Routes = [
     //         component: UpgradeComponent
     //     }]
     // }
-    { path: 'dashboard',      component: DashboardComponent },
-    { path: 'user-profile',   component: UserProfileComponent },
-    { path: 'table-list',     component: TableListComponent },
-    { path: 'typography',     component: TypographyComponent },
-    { path: 'icons',          component: IconsComponent },
-    { path: 'maps',           component: MapsComponent },
-    { path: 'notifications',  component: NotificationsComponent },
-    { path: 'upgrade',        component: UpgradeComponent },
+    //{ path: 'dashboard',      component: DashboardComponent },
 ];
